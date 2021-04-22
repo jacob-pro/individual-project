855c78c4596c2bdb57f327f3e90078d40221e7a3
Jacob Halsey <jacob@jhalsey.com>
2021-04-22 22:06:42 +0100

    log script output

--- kvm-compose/assets/cirros_init.sh
+++ kvm-compose/assets/cirros_init.sh
@@ -4,6 +4,7 @@ cirros-query get public_ssh_key > /home/cirros/.ssh/authorized_keys
 chmod 600 /home/cirros/.ssh/authorized_keys
 chown cirros /home/cirros/.ssh/authorized_keys
 
-cirros-query get run_script > run_script.sh
-chmod +x run_script.sh
-sh run_script.sh
+mkdir -p /etc/nocloud
+cirros-query get run_script > /etc/nocloud/run_script.sh
+chmod +x /etc/nocloud/run_script.sh
+sh /etc/nocloud/run_script.sh &> /etc/nocloud/run_script_log.txt
