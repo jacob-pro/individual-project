f3705798ec6a670fd2b39d13abcad810e3b603da
Jacob Halsey <jacob@jhalsey.com>
2021-04-26 17:28:19 +0100

    update backend.sh

--- dp3t/backend.sh
+++ dp3t/backend.sh
@@ -4,11 +4,22 @@ sudo apt update
 sudo apt install -y git make maven
 mkdir -p /opt/dp3t
 cd /opt/dp3t
-git clone https://github.com/DP-3T/dp3t-sdk-backend.git
+# Temporary fix which doesn't rely on JCenter / Bintray
+# git clone https://github.com/DP-3T/dp3t-sdk-backend.git
+git clone https://github.com/jacob-pro/dp3t-sdk-backend.git
 cd dp3t-sdk-backend/dpppt-backend-sdk
+# Use pre-standard server
+# git checkout v1.1.2
+git checkout v1.1.2-fix
 mvn install -DskipTests
-echo "ws.origin.country=uk" >> application.properties
-echo "spring.profiles.active=dev" >> application.properties
+
+properties="
+ws.origin.country=uk
+spring.profiles.active=dev
+ws.ecdsa.credentials.privateKey=LS0tLS1CRUdJTiBQUklWQVRFIEtFWS0tLS0tCk1JR1RBZ0VBTUJNR0J5cUdTTTQ5QWdFR0NDcUdTTTQ5QXdFSEJIa3dkd0lCQVFRZ0IxQ0E4QmhkVENhQzkvMWoKbTZVcFNhQXlTL01Kc3ZhdmJIQldWaDc5SCtHZ0NnWUlLb1pJemowREFRZWhSQU5DQUFSNmg5UXVhTi9VelZ4WgphUDBFV2h3Zm8yRUs5aUNUbWQyWWhrQ0NKTzY2QjM3VWxGdVpseGxnS2w2cENZTGNVK1pzMDZ3eERYOUI0dFcwCm14WG1nc09CCi0tLS0tRU5EIFBSSVZBVEUgS0VZLS0tLS0K
+ws.ecdsa.credentials.publicKey=LS0tLS1CRUdJTiBQVUJMSUMgS0VZLS0tLS0KTUZrd0V3WUhLb1pJemowQ0FRWUlLb1pJemowREFRY0RRZ0FFZW9mVUxtamYxTTFjV1dqOUJGb2NINk5oQ3ZZZwprNW5kbUlaQWdpVHV1Z2QrMUpSYm1aY1pZQ3BlcVFtQzNGUG1iTk9zTVExL1FlTFZ0SnNWNW9MRGdRPT0KLS0tLS1FTkQgUFVCTElDIEtFWS0tLS0tCg==
+"
+echo "$properties" > application.properties
 
 service="[Unit]
 Description=dp3t backend
