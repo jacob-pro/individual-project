151b1def705b2facbc95f790088cb0adfec38a25
Jacob Halsey <jacob@jhalsey.com>
2021-05-18 21:51:33 +0100

    websocket fixes

--- examples/signal/client.sh
+++ examples/signal/client.sh
@@ -6,10 +6,11 @@ mkdir -p /opt/signal
 cd /opt/signal
 git clone https://github.com/AsamK/signal-cli.git
 cd signal-cli
+git checkout 5c3fc44d00cb7b18c4c5b4b6b5d7fe09f18973db
 git apply --ignore-space-change --ignore-whitespace /etc/nocloud/context/use_custom_server.patch
 ./gradlew build
 ./gradlew installDist
 
 # cd /opt/signal/signal-cli/build/install/signal-cli/bin
 # ./signal-cli -u +447722000001 register
-# ./signal-cli -u +447722000001 verify 111-111
+# ./signal-cli -u +447722000001 verify 111111
