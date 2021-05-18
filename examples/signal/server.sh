e8068ad63e649ed5b8e96403399101a58d2f236a
Jacob Halsey <jacob@jhalsey.com>
2021-05-18 22:56:30 +0100

    signal server in testbed again

--- examples/signal/server.sh
+++ examples/signal/server.sh
@@ -1,15 +1,23 @@
-# Guide to setup Signal Server
-# Must be on an Ubuntu AWS Instance
-# AWS Instance must have an IAM role
-# Role must access a deployed AWS AppConfig with Environment
+set -e
+set -x
 
-# Copy config files
-scp ./config.yml ubuntu@${SIGNAL_IP}:/home/ubuntu/
-scp ./nginx.conf ubuntu@${SIGNAL_IP}:/home/ubuntu/
-
-# Download and build server
-sudo su
 apt update
+
+# Docker
+apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+apt-get install -y docker-ce docker-ce-cli containerd.io
+
+# Signal
 apt install -y git maven
 mkdir -p /opt/signal
 cd /opt/signal
@@ -18,19 +26,26 @@ cd Signal-Server
 git checkout v4.97
 mvn -DskipTests package
 
-# Install Docker + Dependencies
-https://docs.docker.com/engine/install/ubuntu/
-cd /opt/signal
-docker run -d --name accountdb -e "POSTGRES_PASSWORD=postgres" -e "POSTGRES_DB=accountdb" -p 5432:5432 postgres:11
-docker run -d --name abusedb -e "POSTGRES_PASSWORD=postgres" -e "POSTGRES_DB=abusedb" -p 5433:5432 postgres:11
-docker run -d --name messagedb -e "POSTGRES_PASSWORD=postgres" -e "POSTGRES_DB=messagedb" -p 5434:5432 postgres:11
+# Containers
+docker run -d --restart unless-stopped --name accountdb -e "POSTGRES_PASSWORD=postgres" -e "POSTGRES_DB=accountdb" -p 5432:5432 postgres:11
+docker run -d --restart unless-stopped --name abusedb -e "POSTGRES_PASSWORD=postgres" -e "POSTGRES_DB=abusedb" -p 5433:5432 postgres:11
+docker run -d --restart unless-stopped --name messagedb -e "POSTGRES_PASSWORD=postgres" -e "POSTGRES_DB=messagedb" -p 5434:5432 postgres:11
+docker run -d --restart unless-stopped --name redis -e "IP=0.0.0.0" -p 7000-7005:7000-7005 grokzen/redis-cluster:latest
+docker run -d --restart unless-stopped --name nginx --net="host" -v /etc/nocloud/context/nginx.conf:/etc/nginx/nginx.conf:ro nginx
 
-docker run -d --name redis -e "IP=0.0.0.0" -p 7000-7005:7000-7005 grokzen/redis-cluster:latest
-docker run -d --name nginx --net="host" -v /home/ubuntu/nginx.conf:/etc/nginx/nginx.conf:ro nginx
-
-# Run Server
-cd /opt/signal/Signal-Server/service/target
-java -jar TextSecureServer-4.97.jar accountdb migrate /home/ubuntu/config.yml
-java -jar TextSecureServer-4.97.jar abusedb migrate /home/ubuntu/config.yml
-java -jar TextSecureServer-4.97.jar messagedb migrate /home/ubuntu/config.yml
-java -jar -Ddw.logging.level=ERROR TextSecureServer-4.97.jar server /home/ubuntu/config.yml
+# Service
+service="[Unit]
+Description=Signal service
+[Service]
+ExecStart=/usr/bin/java -jar -Ddw.logging.level=ERROR /opt/signal/Signal-Server/service/target/TextSecureServer-4.97.jar server /etc/nocloud/context/config.yml
+WorkingDirectory=/opt/signal/Signal-Server/service/target/
+Type=simple
+Restart=on-failure
+RestartSec=10
+[Install]
+WantedBy=multi-user.target
+"
+echo "$service" > /etc/systemd/system/signal.service
+systemctl daemon-reload
+systemctl enable signal.service
+systemctl start signal.service
